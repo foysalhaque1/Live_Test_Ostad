0e3b0a4567b27f007fca3fd2d985a11a155fa5c7
foysalhaqueshifat <sufishifat@gmail.com>
2024-09-27 23:33:27 +0600

    class file added

--- bin/live_test_01.dart
+++ bin/live_test_01.dart
@@ -1,7 +1,7 @@
 
 import 'class.dart';
 void main() {
-Car car=Car(brand: 'Toyota', model: 'Corola', year: 2015);
+Car car=Car(brand: 'Toyota', model: 'Corolla', year: 2015);
 car.display();
 car.carAge();
 
